359a293221e334404386f35af3720c6b0ecd224a
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2026-04-28 12:42:28 +0900

    init : springboot init

---
+++ src/main/java/com/newdawn/tagvote/TagvoteApplication.java
@@ -0,0 +1,13 @@
+package com.newdawn.tagvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagvoteApplication.class, args);
+	}
+
+}
